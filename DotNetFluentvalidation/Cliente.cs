141bb1b869b3a870710454d3da495326af23dacd
Andrew Santos Maia <andrew.maia@talkcomm.com.br>
2023-06-09 08:14:31 -0300

    Messagee

---
+++ DotNetFluentvalidation/Cliente.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetFluentvalidation
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+            Endereco = "Meu Endereço";
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Desconto { get; set; }
+        public string Endereco { get; set; }
+    }
+
+    public class ClienteValidator : AbstractValidator<Cliente>
+    { 
+        public ClienteValidator()
+        {
+            RuleFor(customer => customer.Nome).NotNull();
+            RuleFor (customer => customer.Endereco).NotEqual("Meu Endereço").WithMessage("Ei este endereço não pode ser utilizado!"); ;
+        }
+    }
+}
---
+++ DotNetFluentvalidation/Program.cs
@@ -0,0 +1,12 @@
+﻿using DotNetFluentvalidation;
+using FluentValidation.Results;
+
+Cliente cliente = new Cliente();
+ClienteValidator validator= new ClienteValidator();
+ValidationResult  result = validator.Validate(cliente); 
+
+if(!result.IsValid)
+{
+    foreach (var erro in result.Errors)
+        Console.WriteLine($"Erro na propriedade {erro.PropertyName}:{erro.ErrorMessage}");
+}
