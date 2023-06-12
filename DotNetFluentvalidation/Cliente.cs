9395cf944129065c3a0f0e585e7396bfcb6105f2
Andrew Santos Maia <andrew.maia@talkcomm.com.br>
2023-06-12 08:45:27 -0300

    Ajustes

--- DotNetFluentvalidation/Cliente.cs
+++ DotNetFluentvalidation/Cliente.cs
@@ -12,12 +12,21 @@ namespace DotNetFluentvalidation
         public Cliente()
         {
             Endereco = "Meu Endereço";
+            TipoCliente = (TipoCliente)3;
         }
 
         public int Id { get; set; }
         public string Nome { get; set; }
         public decimal Desconto { get; set; }
         public string Endereco { get; set; }
+
+        public TipoCliente TipoCliente { get; set; }
+    }
+
+    public enum TipoCliente
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
     }
 
     public class ClienteValidator : AbstractValidator<Cliente>
@@ -25,7 +34,12 @@ namespace DotNetFluentvalidation
         public ClienteValidator()
         {
             RuleFor(customer => customer.Nome).NotNull();
-            RuleFor (customer => customer.Endereco).NotEqual("Meu Endereço").WithMessage("Ei este endereço não pode ser utilizado!"); ;
+            RuleFor (customer => customer.Endereco).NotEqual("Meu Endereço").WithMessage("Ei este endereço não pode ser utilizado!");
+            RuleFor(x => x.TipoCliente).IsInEnum();
+            RuleFor(x => x.Id).Must(id =>
+            {
+                return id == 1;
+            });
         }
     }
 }
--- DotNetFluentvalidation/Program.cs
+++ DotNetFluentvalidation/Program.cs
@@ -3,7 +3,8 @@ using FluentValidation.Results;
 
 Cliente cliente = new Cliente();
 ClienteValidator validator= new ClienteValidator();
-ValidationResult  result = validator.Validate(cliente); 
+ValidationResult  result = validator.Validate(cliente);
+
 
 if(!result.IsValid)
 {
